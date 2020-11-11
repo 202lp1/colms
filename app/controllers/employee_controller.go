f48ed8496404c49ab1cc3dbec0a6fed954f61479
a sullon m <asullom@gmail.com>
2020-11-11 12:20:38 -0500

    employee list

--- app/controllers/employee_controller.go
+++ app/controllers/employee_controller.go
@@ -11,7 +11,50 @@ import (
 	"gorm.io/gorm"
 )
 
+type ViewData struct {
+	Name string
+	//Widgets []models.Empleado
+}
+
 var tmple = template.Must(template.ParseFiles("web/Header.tmpl", "web/Menu.tmpl", "web/Footer.tmpl", "web/employee/index.html"))
+
+func EmployeeList(w http.ResponseWriter, req *http.Request) {
+	lis := []models.Empleado{}
+
+	/////////
+
+	dsn := "docker:docker@tcp(mysql-db:3306)/test_db?charset=utf8mb4&parseTime=True&loc=Local"
+	//dsn := "root:paswd@tcp(localhost:3306)/test_db?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Printf("failed to connect database %v", err)
+	}
+	///////
+
+	log.Printf("db is connected: %v", db)
+
+	// Migrate the schema
+	db.AutoMigrate(&models.Empleado{})
+
+	// Create
+	//db.Create(&models.Empleado{Name: "Juan", City: "Juliaca"})
+
+	db.Find(&lis)
+
+	log.Printf("lis: %v", lis)
+	//d := ViewData{Name: "Angel"}
+
+	//for lis {
+
+	//}
+	fmt.Fprintf(w, "%+v", lis)
+	/*d := models.Empleado{Name: "Angel", City: "Juliaca"}
+	err := tmple.ExecuteTemplate(w, "employee/indexPage", d)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}*/
+}
 
 func EmployeeGet(w http.ResponseWriter, req *http.Request) {
 
@@ -22,30 +65,5 @@ func EmployeeGet(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-}
 
-func EmployeeList(w http.ResponseWriter, req *http.Request) {
-	dsn := "docker:docker@tcp(mysql-db:3306)/test_db?charset=utf8mb4&parseTime=True&loc=Local"
-	//dsn := "root:paswd@tcp(localhost:3306)/test_db?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Printf("failed to connect database %v", err)
-	}
-	log.Printf("db is connected: %v", db)
-
-	// Migrate the schema
-	db.AutoMigrate(&models.Empleado{})
-
-	// Create
-	//db.Create(&models.Empleado{Name: "Juan", City: "Juliaca"})
-	//db.Create(&models.Empleado{Name: "Juan 2", City: "Juliaca2"})
-
-	var lis []models.Empleado
-	db.Find(&lis)
-
-	log.Printf("lis: %v", lis)
-	//for lis {
-
-	//}
-	fmt.Fprintf(w, "%+v", lis)
 }
